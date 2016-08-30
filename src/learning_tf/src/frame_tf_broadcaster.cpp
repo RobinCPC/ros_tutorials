91db8109ece31efc6c4d718c23d7b6a67919eeda
robincpc <chienpinchen@gmail.com>
2016-08-30 16:41:53 -0700

    Add a tf frame (c++)

---
+++ src/learning_tf/src/frame_tf_broadcaster.cpp
@@ -0,0 +1,26 @@
+// Add a frame (c++)
+// source: http://wiki.ros.org/tf/Tutorials/Adding%20a%20frame%20(C++)
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "my_tf_broadcaster");
+  ros::NodeHandle node;
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+
+  ros::Rate rate(10.0);
+  while (node.ok()) 
+  {
+    transform.setOrigin(tf::Vector3(2.0*sin(ros::Time::now().toSec()), 
+          2.0*cos(ros::Time::now().toSec()), 0.0));  
+    transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), 
+          "turtle1", "carrot1"));
+    rate.sleep();
+  }
+
+  return 0;
+}
--- src/learning_tf/src/turtle_tf_listener.cpp
+++ src/learning_tf/src/turtle_tf_listener.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     tf::StampedTransform transform;
     try
     {
-      listener.lookupTransform("/turtle2", "/turtle1",
+      listener.lookupTransform("/turtle2", "/carrot1",
           ros::Time(0), transform);
     }
     catch (tf::TransformException &ex)
